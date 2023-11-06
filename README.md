bfa05287555a082262406c01b18ff6157d6906b6
juliana <julianavido@outlook.com>
2023-11-05 16:49:30 -0800

    edited readme

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 Back-End to an API construction, using Node, Express, JWT and Mongoose.
 It's a part of the final project the Web Development Bootcamp at IronHack.
 
-## About template Model
-
-The general template was built and provided by Teacher [Alexandre Lima](https://github.com/alexandreatlima/rest-api-template).
-
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,11 +3,13 @@ import isAuth from "../middlewares/isAuth.js";
 import attachCurrentUser from "../middlewares/attachCurrentUser.js";
 import { PostModel } from "../model/post.model.js";
 import { UserModel } from "../model/user.model.js";
+import { CommentModel } from "../model/comment.model.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/", isAuth, attachCurrentUser, async (req, res) => {
   try {
+    
     const post = await PostModel.create({
       ...req.body,
       creator: req.currentUser._id,
@@ -59,5 +61,37 @@ postRouter.get("/:id", async (req, res) => {
     return res.status(400).json(e);
   }
 });
+
+postRouter.delete("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedInUser = req.currentUser;
+    const post = await PostModel.findOne({
+      _id: req.params.postId,
+    }).populate("referencePost");
+
+    if ( !post.creator === req.currentUser._id ) {
+      return res.status(401).json("Voce não tem autorização para excluir")
+    }
+    const deletePost = await PostModel.deleteOne({
+      _id: req.params.postId,
+    });
+
+    await CommentModel.deleteMany(
+      { _id: req.params.postId },
+      { $push: { posts: post._id } },
+      { runValidators: true}
+    )
+
+    await UserModel.findByIdAndUpdate(
+      { _id: loggedInUser.postId },
+      { $push: { posts: post._id } },
+      { runValidators: true}
+    )
+      return res.status(200).json(deletePost)
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json(e);
+  }
+})
 
 export { postRouter };
